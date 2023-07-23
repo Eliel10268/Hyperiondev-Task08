95f68f121ca331eb857eb33a45da3f23785d61a9
Eliel Ndayumvire <ndaeliel123@gmail.com>
2023-07-23 22:16:20 +0100

    Task 08

---
+++ task3.py
@@ -0,0 +1,7 @@
+
+#This will loop from 1 to 5(inclusive)
+for i in range(1, 6):
+    #this will create  string of i asterisks obtained from multiplying the string '*' by the value of i
+    asterisks = '*' * i
+    #this will output asteriks to the console
+    print(asterisks)
